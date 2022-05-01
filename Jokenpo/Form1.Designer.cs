8b0242c2ebd49a87a744aedcab7a8c9c8c3b403b
lucasconde <lucasconde36@gmail.com>
2022-05-01 16:34:57 -0300

    Adicionar arquivos de projeto.

---
+++ Jokenpo/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace Jokenpo
+{
+    partial class lblTitulo
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(lblTitulo));
+            this.label1 = new System.Windows.Forms.Label();
+            this.picBox1 = new System.Windows.Forms.PictureBox();
+            this.picBox2 = new System.Windows.Forms.PictureBox();
+            this.picResultado = new System.Windows.Forms.PictureBox();
+            this.labelJogador = new System.Windows.Forms.Label();
+            this.labelResultado = new System.Windows.Forms.Label();
+            this.labelPC = new System.Windows.Forms.Label();
+            this.labelVs = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnPedra = new System.Windows.Forms.Button();
+            this.btnPapel = new System.Windows.Forms.Button();
+            this.btnTesoura = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picResultado)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(776, 75);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Escolha Pedra, Papel ou Tesoura";
+            // 
+            // picBox1
+            // 
+            this.picBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.picBox1.Location = new System.Drawing.Point(22, 94);
+            this.picBox1.Name = "picBox1";
+            this.picBox1.Size = new System.Drawing.Size(200, 200);
+            this.picBox1.TabIndex = 1;
+            this.picBox1.TabStop = false;
+            // 
+            // picBox2
+            // 
+            this.picBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.picBox2.Location = new System.Drawing.Point(305, 94);
+            this.picBox2.Name = "picBox2";
+            this.picBox2.Size = new System.Drawing.Size(200, 200);
+            this.picBox2.TabIndex = 2;
+            this.picBox2.TabStop = false;
+            // 
+            // picResultado
+            // 
+            this.picResultado.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.picResultado.Location = new System.Drawing.Point(588, 94);
+            this.picResultado.Name = "picResultado";
+            this.picResultado.Size = new System.Drawing.Size(200, 200);
+            this.picResultado.TabIndex = 3;
+            this.picResultado.TabStop = false;
+            // 
+            // labelJogador
+            // 
+            this.labelJogador.AutoSize = true;
+            this.labelJogador.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelJogador.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.labelJogador.Location = new System.Drawing.Point(90, 173);
+            this.labelJogador.Name = "labelJogador";
+            this.labelJogador.Size = new System.Drawing.Size(65, 25);
+            this.labelJogador.TabIndex = 4;
+            this.labelJogador.Text = "Você";
+            // 
+            // labelResultado
+            // 
+            this.labelResultado.AutoSize = true;
+            this.labelResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelResultado.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.labelResultado.Location = new System.Drawing.Point(658, 173);
+            this.labelResultado.Name = "labelResultado";
+            this.labelResultado.Size = new System.Drawing.Size(25, 25);
+            this.labelResultado.TabIndex = 5;
+            this.labelResultado.Text = "?";
+            // 
+            // labelPC
+            // 
+            this.labelPC.AutoSize = true;
+            this.labelPC.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPC.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.labelPC.Location = new System.Drawing.Point(381, 173);
+            this.labelPC.Name = "labelPC";
+            this.labelPC.Size = new System.Drawing.Size(43, 25);
+            this.labelPC.TabIndex = 6;
+            this.labelPC.Text = "PC";
+            // 
+            // labelVs
+            // 
+            this.labelVs.AutoSize = true;
+            this.labelVs.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelVs.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.labelVs.Location = new System.Drawing.Point(241, 173);
+            this.labelVs.Name = "labelVs";
+            this.labelVs.Size = new System.Drawing.Size(42, 25);
+            this.labelVs.TabIndex = 7;
+            this.labelVs.Text = "VS";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.label6.Location = new System.Drawing.Point(531, 173);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(25, 25);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "=";
+            // 
+            // btnPedra
+            // 
+            this.btnPedra.BackgroundImage = global::Jokenpo.Properties.Resources.Pedra;
+            this.btnPedra.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnPedra.Location = new System.Drawing.Point(22, 309);
+            this.btnPedra.Name = "btnPedra";
+            this.btnPedra.Size = new System.Drawing.Size(200, 200);
+            this.btnPedra.TabIndex = 9;
+            this.btnPedra.UseVisualStyleBackColor = true;
+            this.btnPedra.Click += new System.EventHandler(this.btnPedra_Click);
+            // 
+            // btnPapel
+            // 
+            this.btnPapel.BackgroundImage = global::Jokenpo.Properties.Resources.Papel;
+            this.btnPapel.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnPapel.Location = new System.Drawing.Point(305, 309);
+            this.btnPapel.Name = "btnPapel";
+            this.btnPapel.Size = new System.Drawing.Size(200, 200);
+            this.btnPapel.TabIndex = 10;
+            this.btnPapel.UseVisualStyleBackColor = true;
+            this.btnPapel.Click += new System.EventHandler(this.btnPapel_Click);
+            // 
+            // btnTesoura
+            // 
+            this.btnTesoura.BackgroundImage = global::Jokenpo.Properties.Resources.Tesoura;
+            this.btnTesoura.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnTesoura.Location = new System.Drawing.Point(588, 309);
+            this.btnTesoura.Name = "btnTesoura";
+            this.btnTesoura.Size = new System.Drawing.Size(200, 200);
+            this.btnTesoura.TabIndex = 11;
+            this.btnTesoura.UseVisualStyleBackColor = true;
+            this.btnTesoura.Click += new System.EventHandler(this.btnTesoura_Click);
+            // 
+            // lblTitulo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 553);
+            this.Controls.Add(this.btnTesoura);
+            this.Controls.Add(this.btnPapel);
+            this.Controls.Add(this.btnPedra);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.labelVs);
+            this.Controls.Add(this.labelPC);
+            this.Controls.Add(this.labelResultado);
+            this.Controls.Add(this.labelJogador);
+            this.Controls.Add(this.picResultado);
+            this.Controls.Add(this.picBox2);
+            this.Controls.Add(this.picBox1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "lblTitulo";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Jokenpo";
+            ((System.ComponentModel.ISupportInitialize)(this.picBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picResultado)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox picBox1;
+        private System.Windows.Forms.PictureBox picBox2;
+        private System.Windows.Forms.PictureBox picResultado;
+        private System.Windows.Forms.Label labelJogador;
+        private System.Windows.Forms.Label labelResultado;
+        private System.Windows.Forms.Label labelPC;
+        private System.Windows.Forms.Label labelVs;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnPedra;
+        private System.Windows.Forms.Button btnPapel;
+        private System.Windows.Forms.Button btnTesoura;
+    }
+}
+
---
+++ Jokenpo/Game.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jokenpo
+{
+     class Game
+    {
+        public enum Resultado
+        {
+            Ganhar, Perder, Empatar
+        }
+
+        public static Image[] imagens =
+        {
+            Image.FromFile("imagens/Pedra.png"),
+            Image.FromFile("imagens/Papel.png"),
+            Image.FromFile("imagens/Tesoura.png")
+        };
+
+        public Image imgPC { get; private set; }
+        public Image imgJogador { get; private set; }
+
+        public Resultado Jogar(int jogador)
+        {
+            int pc = JogadaPC();
+
+            imgJogador = imagens[jogador];
+            imgPC = imagens[pc];
+
+            if(jogador == pc)
+            {
+                return Resultado.Empatar;
+
+            } else if((jogador == 0 && pc == 2) || (jogador == 1 && pc == 0) || (jogador == 2 && pc == 1))
+            {
+                return Resultado.Ganhar;
+
+            } else
+            {
+                return Resultado.Perder;
+            }
+        }
+
+        private int JogadaPC()
+        {
+           int mil = DateTime.Now.Millisecond;
+           int PcJogou;
+
+            if(mil <= 333)
+            {
+                return 0;
+
+            } else if(mil <= 666 && mil > 333)
+            {
+                return 1;
+
+            } else 
+            {
+                return 2;
+            }
+        }
+    }
+}
